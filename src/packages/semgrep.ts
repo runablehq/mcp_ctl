8d9a1b8154ef63e795368b613685fbba39dcc0a5
Drew Dennison <drew@semgrep.com>
2025-04-09 01:29:29 -0700

    Create semgrep.ts
    Add [Semgrep MCP Server](https://github.com/semgrep/mcp)

---
+++ src/packages/semgrep.ts
@@ -0,0 +1,17 @@
+import { createPackage } from "../create_package";
+
+export const semgrep = createPackage({
+  name: "semgrep",
+  aliases: ["semgrep-mcp"],
+  dependsOn: [],
+  inputs: [],
+  buildConfig(inputs) {
+    const args: string[] = ["semgrep-mcp"];
+    return {
+      name: "semgrep",
+      command: "uvx",
+      args,
+      env: {},
+    };
+  },
+});
