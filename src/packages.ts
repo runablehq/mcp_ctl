4612b8027bac92b11be2479e401027338ade46b4
Shoham <shoham@weavy.ai>
2025-04-09 12:14:23 +0300

    Add Framelink figma developer mcp

--- src/packages.ts
+++ src/packages.ts
@@ -1,7 +1,9 @@
 import { github } from "./packages/github";
 import { playwright } from "./packages/playwright";
+import { figma_developer_mcp } from "./packages/figma_developer_mcp";
 
 export const packages = {
   github,
   playwright,
+  figma_developer_mcp,
 };
