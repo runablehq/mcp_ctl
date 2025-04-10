d2f56c4e3efade2eac8c05f9dbb566152dd799e4
Tran Le Cuong <cuongtl92@gmail.com>
2025-04-10 22:53:06 +0700

    feat: Add Unleash MCP Server (#3)
    * feat: Add Unleash MCP Server
    
    Introduce the Unleash MCP Server package with configuration options for Unleash URL, API token, HTTP port, and transport mechanism. This allows for flexible integration with Unleash services.
    
    * fix package import
    
    ---------
    
    Co-authored-by: CuongTL 🐛 <cuong.tl@kiotviet.com>
    Co-authored-by: cotton <168998965+cloudycotton@users.noreply.github.com>

--- src/packages.ts
+++ src/packages.ts
@@ -1,11 +1,13 @@
 import { github } from "./packages/github";
 import { playwright } from "./packages/playwright";
-import { semgrep } from "./packages/semgrep"
+import { semgrep } from "./packages/semgrep";
 import { figma_developer_mcp } from "./packages/figma_developer_mcp";
+import { unleash } from "./packages/unleash";
 
 export const packages = {
   github,
   playwright,
   semgrep,
+  unleash,
   figma_developer_mcp,
 };
