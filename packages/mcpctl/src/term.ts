73069dd957e88eb63edf3fd52b0d13a45fd7e57e
ayaan khan <ayaan.khan2812@gmail.com>
2025-04-21 18:44:44 +0530

    removed the src unused foldedr

--- packages/mcpctl/src/term.ts
+++
@@ -1,9 +0,0 @@
-// Terminal styling utility (replacement for chalk)
-export const term = {
-  bold: (text: string) => `\x1b[1m${text}\x1b[0m`,
-  green: (text: string) => `\x1b[32m${text}\x1b[0m`,
-  red: (text: string) => `\x1b[31m${text}\x1b[0m`,
-  yellow: (text: string) => `\x1b[33m${text}\x1b[0m`,
-  gray: (text: string) => `\x1b[90m${text}\x1b[0m`,
-  dim: (text: string) => `\x1b[2m${text}\x1b[0m`,
-};
